7a5f131e16e419e75bfbae98a3c09b1423672f47
Eric Faraut <eric.faraut@hotmail.fr>
2025-04-14 11:30:59 +0200

    Routing is cleaned.

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import KasaRouter from "./Routing";
+import Routing from "./Routing";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <KasaRouter />
+        <Routing />
     </StrictMode>
 );
