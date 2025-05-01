054f4078fc30510551e2f2942150570e1c084fbe
Eric Faraut <eric.faraut@hotmail.fr>
2025-05-01 20:17:02 +0200

    Linting is cleaned.

--- src/config/loadFileConfig.ts
+++ src/config/loadFileConfig.ts
@@ -1,8 +1,8 @@
 // Home page lazy loading
-export let bannerBackground: string = "../assets/bh_bckgrd.jpg";
-export let card: string = "../assets/card.jpg";
-export let footerLogo: string = "../assets/df_logo.png";
-export let headerLogo: string = "../assets/dh_logo.png";
+export let bannerBackground: string = '../assets/bh_bckgrd.jpg';
+export let card: string = '../assets/card.jpg';
+export let footerLogo: string = '../assets/df_logo.png';
+export let headerLogo: string = '../assets/dh_logo.png';
 
 initFileConfig();
 
@@ -18,7 +18,6 @@ async function loadFileConfig() {
     try {
         const baseURL = window.location.origin;
         const configPath = `${baseURL}/src/config/files_config.json`;
-        console.log("config path: " + configPath);
         const resConfigFetched = await fetch(configPath);
         if (resConfigFetched.ok) {
             const filePaths = await resConfigFetched.json();
@@ -26,8 +25,8 @@ async function loadFileConfig() {
             card = filePaths.card;
             footerLogo = filePaths.logo_footer_desktop;
             headerLogo = filePaths.logo_header_desktop;
-        } else alert("loadConfig() error : " + resConfigFetched.statusText);
+        } else alert('loadConfig() error : ' + resConfigFetched.statusText);
     } catch (error) {
-        alert("loadConfig() error fetching files_config.json : " + error);
+        alert('loadConfig() error fetching files_config.json : ' + error);
     }
 }
