1b216e80748e40b7ce0a4a7ed51a5f3bbcf227bf
Eric Faraut <eric.faraut@hotmail.fr>
2025-05-14 18:07:07 +0200

    Home page is more responsive. Responsive header logo is to do.

--- src/components/UI/Header/index.tsx
+++ src/components/UI/Header/index.tsx
@@ -1,3 +1,6 @@
+import {useContext} from 'react';
+import {PageContext} from '../../../contexts/PageContext';
+
 import logo_mobile from '../../../assets/images/mh_logo.png';
 
 /**
@@ -11,19 +14,86 @@ import logo_mobile from '../../../assets/images/mh_logo.png';
             src={logo_mobile} ...
  */
 function Header() {
-    return (
+    const page = useContext(PageContext);
+    let underlineHome = false;
+    let underlineAbout = false;
+    switch (page) {
+        case 'home':
+            underlineHome = true;
+            break;
+        case 'about':
+            underlineAbout = true;
+            break;
+    }
+    /*
+    let aHome = '';
+    const aHomeBegin = "<a href = '/' aria-label=\"Allez à l'accueil\"";
+    const underlineClass=" className='underline";
+    const aHomeEnd='>Accueil</a>';
+    underlineHome 
+    ? 
+        aHome = aHomeBegin + underlineClass + aHomeEnd
+    :
+        aHome = aHomeBegin + aHomeEnd
+    ;
+
+    let aAbout = '';
+    const aAboutBegin = "<a href = '/about' aria-label=\"Découvrez-en plus sur nous\"";
+    const aAboutEnd='>À propos</a>';
+    underlineAbout 
+    ? 
+        aAbout = aAboutBegin + underlineClass + aAboutEnd
+    :
+        aAbout = aAboutBegin + aAboutEnd
+    ;*/
+    if(underlineHome && ! underlineAbout) return (
         <header>
             <img 
-                src = { logo_mobile } 
+                src = {logo_mobile} 
                 alt = 'logo desktop' 
-                aria-hidden = 'true' />
+                aria-hidden = 'true' 
+            />
             <nav>
-                <a href = '/' aria-label="Allez à l'accueil">
-                    Accueil
-                </a>
-                <a href = '/about' aria-label = 'Découvrez-en plus sur nous'>
-                    À propos
-                </a>
+                <a href = '/' aria-label="Allez à l'accueil"
+                 className='underline'
+                 >Accueil</a>
+                <a href = '/about' aria-label="Découvrez-en plus sur nous"
+                 className='no-underline'
+                >À propos</a>
+            </nav>
+        </header>
+    );
+    else if (! underlineHome && underlineAbout) return (
+        <header>
+            <img 
+                src = {logo_mobile} 
+                alt = 'logo desktop' 
+                aria-hidden = 'true' 
+            />
+            <nav>
+                <a href = '/' aria-label="Allez à l'accueil"
+                 className='no-underline'
+                 >Accueil</a>
+                <a href = '/about' aria-label="Découvrez-en plus sur nous"
+                 className='underline'
+                >À propos</a>
+            </nav>
+        </header>
+    );
+    else if (! underlineHome && ! underlineAbout) return (
+        <header>
+            <img 
+                src = {logo_mobile} 
+                alt = 'logo desktop' 
+                aria-hidden = 'true' 
+            />
+            <nav>
+                <a href = '/' aria-label="Allez à l'accueil"
+                 className='no-underline'
+                 >Accueil</a>
+                <a href = '/about' aria-label="Découvrez-en plus sur nous"
+                 className='no-underline'
+                >À propos</a>
             </nav>
         </header>
     );
