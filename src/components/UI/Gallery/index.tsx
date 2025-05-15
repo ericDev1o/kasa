f2fdad48a0ae75b05ded2aff06486f81614783ce
Eric Faraut <eric.faraut@hotmail.fr>
2025-05-15 17:57:34 +0200

    Cards are mapped from logements.json.

--- src/components/UI/Gallery/index.tsx
+++ src/components/UI/Gallery/index.tsx
@@ -1,30 +1,29 @@
 import Card from '../Card';
-//import logements from '../../../data/logements.json';
-import card from '../../../assets/images/card.jpg';
+import logements from '../../../data/logements.json';
 
 /**
  * This function displays the home page gallery cards.
- * Foreach cover in the logements.json file create a card with the cover image source path.
- * Use map.
- * @returns the cards with their call to click cover
+ * It maps each logement in logements.json with a Card component having
+ *     cover image source path and
+ *     title
+ * props.
+ * @returns the cards with their call to click titled cover
  */
-function Gallery() {
-    /*let covers  =  [
-        logements.forEach(logement  = > logement.cover)
-    ];*/
-    /*const cards  =  covers.map(coverCTA  = > 
-        <Card cover =  {coverCTA}/>
-        );
-    */
+function Gallery() {   
     return (
         <section className = 'gallery'>
             <article className = 'gallery-background'>
-                <Card cover = {card} title = 'Titre de la location' />
-                <Card cover = {card} title = 'Titre de la location' />
-                <Card cover = {card} title = 'Titre de la location' />
-                <Card cover = {card} title = 'Titre de la location' />
-                <Card cover = {card} title = 'Titre de la location' />
-                <Card cover = {card} title = 'Titre de la location' />
+                {
+                    logements.map(
+                        logement => (
+                            <Card 
+                                key={logement.id} 
+                                cover={logement.cover} 
+                                title={logement.title} 
+                            />
+                        )
+                    )
+                }
             </article>
         </section>
     );
