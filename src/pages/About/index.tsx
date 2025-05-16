77d49c820db08ad6a0d6353c04b0cbe7d852bae8
Eric Faraut <eric.faraut@hotmail.fr>
2025-05-16 18:12:05 +0200

    About dropdowns is ongoing.

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -8,11 +8,15 @@ function About() {
     return (
         <Layout>
             <Banner banner = { bannerContent } page = 'about' />
-            <h1>À propos</h1>
-            <h2>Fiabilité</h2>
-            <h2>Respect</h2>
-            <h2>Service</h2>
-            <h2>Sécurité</h2>
+            <h1 className='headings-map-compliance'>À propos</h1>
+            <h2 className='value__h2'>Fiabilité</h2>
+            <p className='value__p'>Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes.</p>
+            <h2 className='value__h2'>Respect</h2>
+            <p className='value__p'>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturabation du voisinage entraînera une exclusion de notre plateforme.</p>
+            <h2 className='value__h2'>Service</h2>
+            <p className='value__p'>La qualité du service est au cœur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction, que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.</p>
+            <h2 className='value__h2'>Sécurité</h2>
+            <p className='value__p'>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
         </Layout>
     );
 }
