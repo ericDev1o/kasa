fc2f9230ee90a402883fd66b847033dab9558557
Eric Faraut <eric.faraut@hotmail.fr>
2025-06-17 18:10:25 +0200

    Stars is refactored.

--- src/components/UI/HousingComponent.tsx
+++ src/components/UI/HousingComponent.tsx
@@ -1,27 +1,39 @@
 import housingType from '../../types/housingType';
 import Collapse from './Collapse';
-import starFull from'/images/star-active.svg';
+import starFull from '/images/star-active.svg';
 import starEmpty from '/images/star-inactive.svg';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, ReactNode } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const numberOfStars = 5;
 
-const stars = (numberOfActiveStars: number, numberOfInactiveStars: number) => 
+const Star = ({ imgURI }: { imgURI: string }) => (
+    <img
+        className = 'star'
+        src = { imgURI }
+        alt = 'rating star'
+    />
+);
+
+const Stars = (
+    { numberOfActiveStars }: { numberOfActiveStars: number }
+) => 
 {
-    let stars: Array<string> = Array.prototype;
+    let stars: Array<ReactNode> = new Array<ReactNode>(5);
 
     for( let j = 0; j < numberOfActiveStars; j++)
     {
-        stars.push(starFull);
+        stars.push(<Star key = { j } imgURI = { starFull } />);
     }
 
-    for( let j = 5 - numberOfInactiveStars; j += 1; j++)
+    for( let j = 0; j < numberOfStars - numberOfActiveStars; j++)
     {
-        stars.push(starEmpty);
+        stars.push(<Star key = { numberOfStars - j } imgURI = { starEmpty } />);
     }
 
-    return <div className='stars'> { stars }</div>;
+    return (
+        <div className='stars'>{ stars }</div>
+    );
 }
 
 const Tag = ({ tagElement }: { tagElement: string }) => {
@@ -61,83 +73,72 @@ const Equipments = ({ equipments }: { equipments: string[] }) => {
     )
 }
 
-const Slider = (pictures: string[]) => 
+const Slider = ({ pictures }: { pictures: string[]}) => 
 {
+    const numberOfPictures = { pictures }.pictures.length;
+    const [currentIndex, setCurrentIndex] = useState(0);
+    const [fadeClass, setFadeClass] = useState('fade-in');
+    const [slideClass, setSlideClass] = useState('slide-zoom');
     const navigate = useNavigate();
 
-    useEffect(() => {
-        const renderSlider = () => 
-        {
-            try
-            {
-                if(pictures && pictures.length > 0) 
-                {
-                    const [currentIndex, setCurrentIndex] = useState(0);
-                    const [fadeClass, setFadeClass] = useState('fade-in');
-                    const [slideClass, setSlideClass] = useState('slide-zoom');
-                    const numberOfPictures = pictures.length;
-
-                    const nextSlide = () => {
-                        setFadeClass('');
-                        setSlideClass('');
-                        setCurrentIndex((previndex) => (previndex + 1) % numberOfPictures);
-                    }
-
-                    const prevSlide = () => {
-                        setFadeClass('');
-                        setSlideClass('');
-                        setCurrentIndex((prevIndex) => (prevIndex - 1 + numberOfPictures) % numberOfPictures);
-                    }
-
-                    useEffect(() => {
-                        setFadeClass('fade-in');
-                        setSlideClass('slide-zoom');
-                    }, [currentIndex]);
-
-                    return (
-                        <div className='cover-container slider'>
-                        { 
-                            numberOfPictures === 1 
-                            ? 
-                            <img src = { pictures[0] } alt = 'logement' />
-                            : (
-                                <div className='slider-container'>
-                                    <button className = 'arrow left' onClick = { prevSlide }>
-                                        <span className = 'material-symbols-outlined'>arrow_back_ios</span>
-                                    </button>
-                                    <img
-                                        src = { pictures[currentIndex] } 
-                                        alt = 'logement' 
-                                        className = { `cover ${ fadeClass } ${ slideClass }` }
-                                    />
-                                    <div 
-                                        className='counter'
-                                    >
-                                        { currentIndex + 1 } / { numberOfPictures }
-                                    </div>
-                                    <button className='arrow right' onClick = { nextSlide }>
-                                        <span className = 'material-symbols-outlined'>arrow_forward_ios</span>
-                                    </button>
-                                </div>
-                            )
-                        }
-                        </div>
-                    );
-                }
-                else navigate('/error_there_must_be_picture·s)');
-            } catch(error) {
-                if(error instanceof ReferenceError)
-                    navigate('/error');
-                else if (error instanceof TypeError)
-                    navigate('/error_you_must_check_a_type_of_internal_banner_content_or_external_housing_data');
-                else if (error instanceof SyntaxError)
-                    navigate('/error_you_must_check_the_syntax');
-                else if (error instanceof Error)
-                    navigate('/error');
+    try
+    {
+        if(pictures && pictures.length > 0) 
+        {                  
+            const nextSlide = () => {
+                setFadeClass('');
+                setSlideClass('');
+                setCurrentIndex((previndex) => (previndex + 1) % numberOfPictures);
             }
+
+            const prevSlide = () => {
+                setFadeClass('');
+                setSlideClass('');
+                setCurrentIndex((prevIndex) => (prevIndex - 1 + numberOfPictures) % numberOfPictures);
+            }
+
+            useEffect(() => {
+                setFadeClass('fade-in');
+                setSlideClass('slide-zoom');
+            }, [currentIndex]);
+
+            return (
+                <div className='cover-container slider'>
+                { 
+                    numberOfPictures === 1 
+                    ? 
+                    <img src = { pictures[0] } alt = 'logement' />
+                    : (
+                        <div className='slider-container'>
+                            <button className = 'arrow left' onClick = { prevSlide }>
+                                <span className = 'material-symbols-outlined'>arrow_back_ios</span>
+                            </button>
+                            <img
+                                src = { pictures[currentIndex] } 
+                                alt = 'logement' 
+                                className = { `cover ${ fadeClass } ${ slideClass }` }
+                            />
+                            <div 
+                                className='counter'
+                            >
+                                { currentIndex + 1 } / { numberOfPictures }
+                            </div>
+                            <button className='arrow right' onClick = { nextSlide }>
+                                <span className = 'material-symbols-outlined'>arrow_forward_ios</span>
+                            </button>
+                        </div>
+                    )
+                }
+                </div>
+            );
         }
-        renderSlider();
-    }, pictures);
+        else navigate('/error_there_must_be_picture·s)');
+    } catch(error) {
+        if (error instanceof TypeError)
+            navigate('/error_you_must_check_a_type_of_internal_banner_content_or_external_housing_data');
+        else
+            navigate('/error');
+    }
 }
 
 /**
@@ -145,7 +146,7 @@ const Slider = (pictures: string[]) =>
  * @param housingElement a type corresponding to backend API housing content
  * @returns a housing page content with newlined equipments content in the Collapse
  */
-export default function HousingComponent (housingElement: housingType) {
+export default function HousingComponent ({ housingElement }: { housingElement: housingType }) {
     const numberOfActiveStars = Number(housingElement.rating);
 
     return (
@@ -160,11 +161,11 @@ export default function HousingComponent (housingElement: housingType) {
                 </section>
             </div>
             <div className='stars-host'>
-                { stars(numberOfActiveStars, numberOfStars - numberOfActiveStars) }
+                <Stars numberOfActiveStars = { numberOfActiveStars} />
                 <div className='host'>
-                    {/* <p className='value__p font-red'>{ housingElement.host.name }</p> */}
+                    { <p className='value__p font-red'>{ housingElement.host.name }</p> }
                     <img 
-                        src = 'https://s3-eu-west-1.amazonaws.com/course.oc-static.com/projects/front-end-kasa-project/accommodation-20-1.jpg' //{ housingElement.host.picture } 
+                        src = { housingElement.host.picture } 
                         className='host-picture' 
                         alt = 'host picture' />
                 </div>
