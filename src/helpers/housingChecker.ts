60807ec4cfa22f53a2cb24ea765ca95d37bf82ce
Eric Faraut <eric.faraut@hotmail.fr>
2025-06-20 13:46:02 +0200

    Housing rating check is tested: non-regressive.

--- src/helpers/housingChecker.ts
+++ src/helpers/housingChecker.ts
@@ -6,8 +6,18 @@ import housingType from "../types/housingType";
  * else true it is Ok.
  */
 export default function housingRatingChecker_isOk(housing: housingType) {
-    const ratingNumber = Number(housing.rating);
-    if( ratingNumber >= 1 && ratingNumber <= 5)
-        return true;
-    return false;
+    try {
+        const ratingNumber = Number(housing.rating);
+
+        if( typeof ratingNumber == typeof Number && ratingNumber >= 1 && ratingNumber <= 5)
+            return true;
+        return false;
+    }
+    catch(error){
+        const castingError = 'API rating must be a number.';
+
+        if(typeof error == typeof TypeError)
+            throw new TypeError(castingError);
+        else throw new Error(`${ castingError } Please finally check root error message and stack trace.`)
+    }
 }
