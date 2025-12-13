df8b85ffa604c962a76f7d75b7fffe32386efa51
Eric Faraut <eric.faraut@hotmail.fr>
2025-12-13 22:31:28 +0100

    Redirects is added for netlify.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { UserConfig } from 'vite';
+import { defineConfig, UserConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default {
-    plugins: [react()],
-} satisfies UserConfig;
+export default defineConfig({
+    plugins: [react()]
+}) satisfies UserConfig;
